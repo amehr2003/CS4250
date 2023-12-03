3199cb3d2b72582a0bda7c877a32ae3182b821a4
amehr2003 <98427061+amehr2003@users.noreply.github.com>
2023-12-02 18:07:59 -0800

    Update crawler.py

--- Ass3/crawler.py
+++ Ass3/crawler.py
@@ -1,14 +1,96 @@
+import re
 import urllib
 from urllib.request import urlopen
 from urllib.error import HTTPError
 from urllib.error import URLError
-from bs4 import BeautifulSoup as bs
-import pymongo
 import sys, traceback
 import datetime
+import pymongo
+from bs4 import BeautifulSoup
+import requests
 
-url = 'https://www.cpp.edu'
-frontier = 'https://www.cpp.edu/sci/computer-science/faculty-and-staff/permanent-faculty.shtml'
+seed = 'https://www.cpp.edu'
+frontier_list = ['https://www.cpp.edu/sci/computer-science/']
+
+def crawlPage(frontier):
+    httpRq=requests.get(frontier)
+    bs = BeautifulSoup(httpRq.text, 'html.parser')
+
+    links = bs.find_all('a')
+    count=0
+    for link in links:
+        innerLink=link.get("href")
+        if str(innerLink).startswith("http") or str(innerLink).endswith("/sci/computer-science/"):
+            frontier_list.append(innerLink)
+            print(innerLink)
+
+            if innerLink in frontier_list:
+                print("Already visited page. ")
+            else:
+                if innerLink.startswith('/sci/computer-science/'):
+                    string=seed+innerLink
+                frontier_list.append(string)
+            print(frontier_list)
+            title=getPageTitle(innerLink)
+            saveToDB(innerLink, title)
+            if str(title).strip()=='Permanent Faculty':
+                frontier_list.clear()
+                print("target page found.")
+                break
+        count+=1
+        print(count)
+    return 0
+
+
+def saveToDB(innerLink, title):
+    db=connectDataBase()
+    col = db.cppPages
+    create_date=datetime.datetime.now()
+
+    if innerLink.startswith("/sci/computer-science/"):
+        string = seed + innerLink
+    else:
+        string=innerLink
+
+    html=urlopen(string)
+    bs= BeautifulSoup(html.read(), "html.parser")
+    text=bs.find_all('html')
+
+    doc={
+        "url": string,
+        "title": title,
+        "html": str(html),
+        "created": create_date
+    }
+
+
+def getPageTitle(innerLink):
+    if innerLink.startswith('/sci/computer-science/'):
+        urlString = seed + innerLink
+    else:
+        urlString=innerLink
+    htmlPage=urlopen(urlString)
+    bs=BeautifulSoup(htmlPage.read(), "html.parser")
+    headerTag = bs.find('h1', {"class": "cpp-h1"})
+
+    if headerTag:
+        rtnstr=headerTag.get_text()
+    else:
+        rtnstr=''
+
+    return rtnstr
+
+def connectDataBase():
+    try:
+        client = pymongo.MongoClient(host="localhost", port=27017)
+        db = client.pages
+        print("Database: ")
+        print(db)
+        return db
+    except Exception as error:
+        traceback.print_exc()
+        print("Database not connected successfully")
+        return 0
 
 try:
     html = urllib.request.urlretrieve('https://www.cpp.edu/sci/computer-science/')
@@ -18,6 +100,4 @@ except URLError as e:
     print('The server could not be found!')
 else:
     print('It Worked!')
-
-def crawlPage(frontier):
-    return 
+    crawlPage(seed)
